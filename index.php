24e4c7432520241721376babd304b9a2137c8846
DerRiese <DerRiese765@gmail.com>
2023-05-06 17:46:53 +0300

    -

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
+<?php
+$title = "Ahmed Ibrahim - "
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo "Learn PHP 8.2"?></title>
+    <title><?php echo $title . "Home Page"?></title>
 </head>
 <body>
-    <?php echo "<h1>Welcome to my website</h1>" ?>
+    <?php echo "<h1>Welcome to $title</h1>" ?>
 </body>
 </html>
