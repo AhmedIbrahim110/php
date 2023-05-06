3b31725cc790aa39c2df533daa2e914b28b397a1
DerRiese <DerRiese765@gmail.com>
2023-05-06 18:17:34 +0300

    -

--- strings.php
+++ strings.php
@@ -2,5 +2,12 @@
 $string = "Ahmed Ibrahim";
 echo $string;
 echo "<br>";
-echo strlen($string);
+//echo strtolower($string);
+//echo strtoupper($string);
+//echo strlen($string);
+//echo $string[7]
+
+//echo str_replace("Ibrahim", "Faisal", $string);
+//echo substr($string, 6)
+
 ?>
