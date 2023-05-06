107333cc4391b8747c36982e64856c81cd651234
DerRiese <DerRiese765@gmail.com>
2023-05-06 19:09:51 +0300

    -

---
+++ if.php
@@ -0,0 +1,28 @@
+
+<?php 
+/* 
+$number1 =20;
+$number2 =0;
+
+if($number1 > $number2){
+    echo "الرقم الاول اكبر";
+    if($number1 === 20){
+        echo "<br>";
+        echo "الرقم الاول عشرون";
+    }
+}elseif($number1 < $number2){
+    echo "الرقم الاول اصغر";
+}
+else{
+    echo "الرقمين متساويين";
+}*/
+$username = "Ahmed Ibrahim";
+$email = "AI@gmail.com";
+
+if($username === "Ahmed Ibrahim" && $email === "AI@gmail.com"){
+    echo "Welcome Admin";
+}
+
+
+?>
+
